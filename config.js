0b92f7d24daa2954d3b092f0927790080290cc61
KST <kcfnk1001@icloud.com>
2026-09-14 18:38:23 +0900

    Supabase PRD接続設定を追加

---
+++ config.js
@@ -0,0 +1,7 @@
+window.YTM_CONFIG = Object.freeze({
+  supabaseUrl: 'https://gxpthkgvqyagfmbwcodm.supabase.co',
+  supabasePublishableKey: 'sb_publishable_5zetWZw87Mw7sMCFLIQ3NQ_8qLVHU94',
+  storageBucket: 'crew-ledger-private',
+  appName: '協力会社・出面管理',
+  companyName: '山﨑塗装'
+});
